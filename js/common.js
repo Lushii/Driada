f6043c84280be99067432a7b9130024fb9190659
Lushii <antbear9@gmail.com>
2016-02-25 10:38:12 +0300

    add slider

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.slick-slider').slick({
+    setting-name: setting-value
+  });
+});
+$('.single-item').slick();
+$('.one-time').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+        
